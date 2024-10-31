19f0ab2680761d8ddc2946f9bf8aa35b8a4baed5
NicholasCarreiroSecE <n01492047@humbermail.ca>
2024-10-31 13:52:46 -0400

    comments
    added in comments on js and jquery functions

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
-    $('.filter-btn').click(function () {
-        const category = $(this).data('category');
-        $('.gallery-item').each(function () {
-            if (category === 'all' || $(this).data('category') === category) {
-                $(this).fadeIn();
+    $('.filter-btn').click(function () {    // When the filter button is clicked
+        const category = $(this).data('category');  // Get the category value from the data-category attribute
+        $('.gallery-item').each(function () {   // Loop through each gallery item
+            if (category === 'all' || $(this).data('category') === category) { // If the category is 'all' or the data-category value matches the category
+                $(this).fadeIn();   // Show the gallery item
             } else {
-                $(this).fadeOut();
+                $(this).fadeOut();  // Otherwise, hide the gallery item
             }
         });
     });
 
-    $('.gallery-item').click(function () {
-        const imgSrc = $(this).find('img').attr('src');
-        $('#lightbox-image').attr('src', imgSrc);
-        $('#lightbox').fadeIn();
+    $('.gallery-item').click(function () { 
+        const imgSrc = $(this).find('img').attr('src'); // Get the image source from the clicked gallery item
+        $('#lightbox-image').attr('src', imgSrc); // Set the image source in the lightbox
+        $('#lightbox').fadeIn(); // Show the lightbox
     });
 
-    $('#close-lightbox').click(function () {
-        $('#lightbox').fadeOut();
+    $('#close-lightbox').click(function () { // When the close button is clicked
+        $('#lightbox').fadeOut(); // Hide the lightbox
     });
 
-    $('#lightbox').click(function (e) {
-        if (e.target.id === 'lightbox') {
-            $('#lightbox').fadeOut();
+    $('#lightbox').click(function (e) { // When the lightbox is clicked
+        if (e.target.id === 'lightbox') { // If the target element is the lightbox
+            $('#lightbox').fadeOut();  // Hide the lightbox
         }
     });
 });
